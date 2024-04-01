1507c1c1387336d7ad1aeaa9d97a3cf4034cf645
Jin Kisara <nguyentunganh039@gmail.com>
2024-04-01 23:50:17 +0700

    Add mob

--- Mob.gd
+++ Mob.gd
@@ -1,11 +1,20 @@
 extends RigidBody2D
 
+# Random spawn enemies
+@export var min_speed = 150.0
+@export var max_speed = 150.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#$AnimatedSprite2D.play() = true # Version 3
+	$AnimatedSprite2D.play()
+	
+	#var mob_types = $AnimatedSprite2D.frames.get_animation_names() # Version 3
+	var mob_types : Array = $AnimatedSprite2D.sprite_frames.get_animation_names()
+	
+	$AnimatedSprite2D.animation = mob_types[randi() % mob_types.size()]
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	# Destroy an node function at the end of the frame
+	# There is another function called free but that more dangerous
+	queue_free()
